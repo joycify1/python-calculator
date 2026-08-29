2b0406e3d909e4221bec155bb4a17f149915809b
joycify1 <blessyjoyce29@gmail.com>
2026-08-29 12:45:12 +0530

    Add calculator script

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+op=input("Enter the operation: "))
+
+if op=='+':
+   print(a+b)
+elif op=='-':
+     print(a-b)
+elif op=='*':
+     print(a*b)
+elif op=='%':
+     print(a%b)
+elif op=='**':
+     print(a**b)
+else:
+     print("Invalid operation.")
